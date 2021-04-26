b8fb6be5952e37128e7d049fdef4b8a9b5bb7976
Leonardo Delmiro <leodelmiroms@gmail.com>
2021-04-26 09:51:23 -0300

    Interceptor 3.0

--- src/main/kotlin/br/com/leodelmiro/compartilhado/exceptions/ExceptionHandlerInterceptor.kt
+++ src/main/kotlin/br/com/leodelmiro/compartilhado/exceptions/ExceptionHandlerInterceptor.kt
@@ -1,23 +1,16 @@
 package br.com.leodelmiro.compartilhado.exceptions
 
-import br.com.leodelmiro.registro.exceptions.ClienteNaoEncontradoException
-import br.com.leodelmiro.registro.exceptions.PixJaExistenteException
-import br.com.leodelmiro.remocao.exceptions.ChaveInexistenteException
-import com.google.rpc.BadRequest
-import com.google.rpc.Code
 import io.grpc.BindableService
-import io.grpc.Status
-import io.grpc.StatusRuntimeException
-import io.grpc.protobuf.StatusProto.toStatusRuntimeException
 import io.grpc.stub.StreamObserver
 import io.micronaut.aop.InterceptorBean
 import io.micronaut.aop.MethodInterceptor
 import io.micronaut.aop.MethodInvocationContext
 import org.slf4j.LoggerFactory
-import javax.validation.ConstraintViolationException
+import javax.inject.Inject
 
 @InterceptorBean(ErrorHandler::class)
-class ExceptionHandlerInterceptor : MethodInterceptor<BindableService, Any> {
+class ExceptionHandlerInterceptor(@Inject private val resolver: ExceptionHandlerResolver) :
+    MethodInterceptor<BindableService, Any> {
 
     private val LOGGER = LoggerFactory.getLogger(this.javaClass)
 
@@ -25,41 +18,16 @@ class ExceptionHandlerInterceptor : MethodInterceptor<BindableService, Any> {
         try {
             return context.proceed()
         } catch (e: Exception) {
-            LOGGER.error(e.message)
+            LOGGER.error("Exceção ${e.javaClass.name} enquanto chamava ${context.targetMethod}", e)
 
-            val statusError = when (e) {
-                is ChaveInexistenteException -> Status.NOT_FOUND.withDescription(e.message).asRuntimeException()
-                is ClienteNaoEncontradoException -> Status.NOT_FOUND.withDescription(e.message).asRuntimeException()
-                is PixJaExistenteException -> Status.ALREADY_EXISTS.withDescription(e.message).asRuntimeException()
-                is IllegalStateException -> Status.FAILED_PRECONDITION.withDescription(e.message).asRuntimeException()
-                is IllegalArgumentException -> Status.INVALID_ARGUMENT.withDescription(e.message).asRuntimeException()
-                is ConstraintViolationException -> handleConstraintValidationException(e)
-                else -> Status.UNKNOWN.withDescription("Erro inesperado aconteceu").asRuntimeException()
-            }
+            @Suppress("UNCHECKED_CAST")
+            val handler = resolver.resolve(e) as ExceptionHandler<Exception>
+            val status = handler.handle(e)
 
             val responseObserver = context.parameterValues[1] as StreamObserver<*>
-            responseObserver.onError(statusError)
+            responseObserver.onError(status.asRuntimeException())
+
             return null
         }
-    }
-
-    private fun handleConstraintValidationException(e: ConstraintViolationException): StatusRuntimeException {
-        val badRequest = BadRequest.newBuilder()
-            .addAllFieldViolations(e.constraintViolations.map {
-                BadRequest.FieldViolation.newBuilder()
-                    .setField(it.propertyPath.last().name)
-                    .setDescription(it.message)
-                    .build()
-            }
-            ).build()
-
-        val statusProto = com.google.rpc.Status.newBuilder()
-            .setCode(Code.INVALID_ARGUMENT_VALUE)
-            .setMessage("Request com parametros inválidos")
-            .addDetails(com.google.protobuf.Any.pack(badRequest))
-            .build()
-
-        LOGGER.info("$statusProto")
-        return toStatusRuntimeException(statusProto)
     }
 }
---
+++ src/main/kotlin/br/com/leodelmiro/compartilhado/exceptions/handlers/ConstraintViolationExceptionHandler.kt
@@ -0,0 +1,33 @@
+package br.com.leodelmiro.compartilhado.exceptions.handlers
+
+import br.com.leodelmiro.compartilhado.exceptions.ExceptionHandler
+import com.google.rpc.BadRequest
+import com.google.rpc.Code
+import javax.inject.Singleton
+import javax.validation.ConstraintViolationException
+
+@Singleton
+class ConstraintViolationExceptionHandler : ExceptionHandler<ConstraintViolationException> {
+    override fun handle(e: ConstraintViolationException): ExceptionHandler.StatusWithDetails {
+        val details = BadRequest.newBuilder()
+            .addAllFieldViolations(e.constraintViolations.map {
+                BadRequest.FieldViolation.newBuilder()
+                    .setField(it.propertyPath.last().name)
+                    .setDescription(it.message)
+                    .build()
+            }
+            ).build()
+
+        val statusProto = com.google.rpc.Status.newBuilder()
+            .setCode(Code.INVALID_ARGUMENT_VALUE)
+            .setMessage("Request com parametros inválidos")
+            .addDetails(com.google.protobuf.Any.pack(details))
+            .build()
+
+        return ExceptionHandler.StatusWithDetails(statusProto)
+    }
+
+    override fun supports(e: Exception): Boolean {
+        return e is ConstraintViolationException
+    }
+}
